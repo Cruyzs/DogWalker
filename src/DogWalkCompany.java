471b90fe3fad5e2fa4cf03b41613a462ba608f62
Cruz English <Cruze3@nycstudents.net>
2025-09-26 12:14:35 -0400

    fixed errors

--- src/DogWalkCompany.java
+++ src/DogWalkCompany.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DogWalkCompany {
+    private int[] schedule;
+
+    public DogWalkCompany() {
+        schedule = new int[24];
+    }
+
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < schedule.length; i++)
+            s += i + " " + schedule[i] + "\n";
+        return s;
+    }
+
+    public int addDogs() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("what hour?");
+        int hour = s.nextInt();
+        System.out.println("how many dogs?");
+        int dogs = s.nextInt();
+        schedule[hour] = dogs;
+        return hour;
+    }
+    /**
+     * Returns the number of dogs, always greater than 0, that are available
+     * for a walk during the time specified by hour
+     * Precondition: 0 <= hour <= 23
+     */
+    public int numAvailableDogs(int hour)
+    {
+        return schedule[hour];
+    }
+    /**
+     * decreases, by numberDogsWalked, the number of dogs available for a walk
+     * during the time specified by hour
+     * numberDogsWalker > 0
+     */
+    public void updateDogs(int hour, int numberDogsWalked)
+    {
+        schedule[hour] -= numberDogsWalked;
+    }
 }
--- src/DogWalker.java
+++ src/DogWalker.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+public class DogWalker
+{
+    /**
+     * the maximum number of dogs this walker can walk simultaneously
+     * per hour
+     */
+    private int maxDogs;
+    /** The dog-walking company this dog walker is associated with */
+    private DogWalkCompany company;
 
-public class DogWalker {
+    /**
+     * Assigns max to maxDogs and comp to company
+     * Precondition: max > 0
+     */
+    public DogWalker(int max, DogWalkCompany comp)
+    {
+        maxDogs = max;
+        company = comp;
+    }
+
+    public int walkDogs(int hour)
+    {
+        int dogs = company.numAvailableDogs(hour);
+        if (dogs < maxDogs)
+        {
+            company.updateDogs(hour, dogs);
+            return dogs;
+        }
+        else
+        {
+           company.updateDogs(hour, maxDogs);
+           return maxDogs;
+        }
+    }
+    public int dogWalkShift(int startHour, int endHour)
+    {
+        int pay = 0;
+        while(startHour <= endHour)
+        {
+            int dogs = walkDogs(startHour);
+        pay += dogs * 5;
+        if (dogs == maxDogs || startHour >= 9 && startHour <= 17)
+            pay += 3;
+        startHour++;
+        }
+        return pay;
+    }
 }
